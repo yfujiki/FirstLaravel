cccc85e0644870532340404e286714e113402cf0
Yuichi Fujiki <fjk89025@gmail.com>
2023-02-03 00:04:24 +0900

    Support edit/update

--- forThird/resources/views/chirps/index.blade.php
+++ forThird/resources/views/chirps/index.blade.php
@@ -21,7 +21,24 @@
                     </div>
                     <p class="mt-4 text-lg text-gray-900">{{ $chirp->message }}</p>
                 </div>
+                @if ($chirp->user->is(auth()->user()))
+                <x-dropdown>
+                    <x-slot name="trigger">
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                            </svg>
+                        </button>
+                    </x-slot>
+                    <x-slot name="content">
+                        <x-dropdown-link :href="route('chirps.edit', $chirp)">
+                            {{ __('Edit') }}
+                        </x-dropdown-link>
+                    </x-slot>
+                </x-dropdown>
+                @endif
             </div>
+
             @endforeach
         </div>
     </div>
