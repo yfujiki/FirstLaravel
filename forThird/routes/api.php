596e2395a30b6e7aebb2a14a014fb6b0e81cf109
Yuichi Fujiki <fjk89025@gmail.com>
2023-02-10 10:38:32 +0900

    Thorough implementation of api auth

--- forThird/routes/api.php
+++ forThird/routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Validation\Validator;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +20,57 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/login', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password) || !$user->forApp) {
+        return response()->json([
+            'status' => false,
+            'message' => 'validation error',
+            'errors' => 'The provided credentials are incorrect.'
+        ], 403);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
+
+Route::post('register', function (Request $request) {
+    //Validated
+    $validateUser = $request->validate(
+        [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required',
+            'device_name' => 'required'
+        ]
+    );
+
+    // if ($validateUser->fails()) {
+    //     return response()->json([
+    //         'status' => false,
+    //         'message' => 'validation error',
+    //         'errors' => $validateUser->errors()
+    //     ], 401);
+    // }
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+        'forApp' => true
+    ]);
+
+    return response()->json([
+        'status' => true,
+        'message' => 'User Created Successfully',
+        'token' => $user->createToken($request->device_name)->plainTextToken
+    ], 200);
 });
