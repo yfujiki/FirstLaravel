3f047f299752bbf2a889400c7b55df797b9b4dfb
Yuichi Fujiki <fjk89025@gmail.com>
2023-03-26 14:15:52 +0900

    Add script to start/stop cloud

---
+++ start-third.sh
@@ -0,0 +1,2 @@
+gcloud --project=resourceloadfailure sql instances patch for-third --activation-policy=ALWAYS
+gcloud --project=resourceloadfailure compute instances start third
---
+++ stop-third.sh
@@ -0,0 +1,2 @@
+gcloud --project=resourceloadfailure sql instances patch for-third --activation-policy=NEVER
+gcloud --project=resourceloadfailure compute instances stop third
