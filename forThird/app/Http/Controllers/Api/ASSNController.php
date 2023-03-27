81feff4522137ba73081c83f456fb3d19dcf3ce9
Yuichi Fujiki <fjk89025@gmail.com>
2023-03-27 21:53:58 +0900

    Added logging for transaction check

--- forThird/app/Http/Controllers/Api/ASSNController.php
+++ forThird/app/Http/Controllers/Api/ASSNController.php
@@ -21,62 +21,70 @@ class ASSNController extends Controller
     {
         $signedResponse = json_decode($request->getContent());
 
-        foreach ($signedResponse as $key => $value) {
-            if ($key == "signedPayload") {
-                // $payload = ASSNController::decode_signed_response($value);
-                list($header, $payload, $signature) = explode('.', $value);
-                $header = JWT::jsonDecode(JWT::urlsafeB64Decode($header));
-                $payload = JWT::jsonDecode(JWT::urlsafeB64Decode($payload));;
-                // Log::info(var_export($header, true));
-                $root_cert = Storage::get('AppleWWDRCAG3_public.pem');
-                // Log::info($root_cert);
+        $signedPaylod = $signedResponse->signedPayload;
+        list($header, $payload, $signature) = explode('.', $signedPaylod);
+        $header = JWT::jsonDecode(JWT::urlsafeB64Decode($header));
+        $payload = JWT::jsonDecode(JWT::urlsafeB64Decode($payload));;
+        $root_cert = Storage::get('AppleWWDRCAG3_public.pem');
 
-                foreach ($payload as $pkey => $pvalue) {
-                    if ($pkey == 'notificationType') {
-                        Log::info("notificationType: " . $pvalue);
-                    }
-                    if ($pkey == 'subtype') {
-                        Log::info("subtype: " . $pvalue);
-                    }
-                    if ($pkey == 'data') {
-                        foreach ($pvalue as $dkey => $dvalue) {
-                            if ($dkey == 'signedTransactionInfo') {
-                                $signedTransactionInfo = $dvalue;
-                                list($theader, $tpayload, $tsignature) = explode('.', $signedTransactionInfo);
+        $notificationType = $payload->notificationType;
+        $subtype = $payload->subtype;
 
-                                $theader = JWT::jsonDecode(JWT::urlsafeB64Decode($theader));
-                                $tpayload = JWT::jsonDecode(JWT::urlsafeB64Decode($tpayload));;
+        Log::info("notificationType: " . $notificationType);
+        Log::info("subtype: " . $subtype);
 
-                                if ($header == $theader) {
-                                    Log::info("Transaction header is the same");
-                                }
+        $data = $payload->data;
 
-                                // Log::info(var_export($theader, true));
-                                Log::info(var_export($tpayload, true));
-                            }
-                            if ($dkey == 'signedRenewalInfo') {
-                                $signedRenewalInfo = $dvalue;
-                                list($rheader, $rpayload, $rsignature) = explode('.', $signedRenewalInfo);
+        // transaction
+        $signedTransactionInfo = $data->signedTransactionInfo;
 
-                                $rheader = JWT::jsonDecode(JWT::urlsafeB64Decode($rheader));
-                                $rpayload = JWT::jsonDecode(JWT::urlsafeB64Decode($rpayload));;
+        list($theader, $tpayload, $tsignature) = explode('.', $signedTransactionInfo);
 
-                                if ($header == $rheader) {
-                                    Log::info("Renewal header is the same");
-                                }
+        $theader = JWT::jsonDecode(JWT::urlsafeB64Decode($theader));
+        $tpayload = JWT::jsonDecode(JWT::urlsafeB64Decode($tpayload));;
 
-                                // Log::info(var_export($rheader, true));
-                                Log::info(var_export($rpayload, true));
-                            }
-                        }
-                    }
-                }
-
-                // $last_cert_idx = count($header->x5c) - 1;
-                // $last_cert = $header->x5c[$last_cert_idx];
-                // Log::info($last_cert);
-            }
+        if ($header == $theader) {
+            Log::info("Transaction header is the same");
         }
-        return "hello";
+
+        Log::info("Origianl transaction id: " . $tpayload->original_transaction_id);
+
+        if ($notificationType == 'SUBSCRIBED') {
+            Log::info("SUBSCRIBED to " . $tpayload->product_id);
+        }
+        if ($notificationType == 'DID_CHANGE_RENEWAL_PREF' && $subtype == 'UPGRADE') {
+            Log::info("UPGRADE to " . $tpayload->product_id);
+        }
+        if ($notificationType == 'DID_CHANGE_RENEWAL_PREF' && $subtype == 'DOWNGRADE') {
+            Log::info("DOWNGRADE to " . $tpayload->product_id);
+        }
+        if ($notificationType == 'DID_RENEW') {
+            Log::info("DID_RENEW for " . $tpayload->product_id);
+        }
+        if ($notificationType == 'EXPIRED') {
+            Log::info($tpayload->product_id . " EXPIRED");
+        }
+        if ($notificationType == 'GRACE_PERIOD_EXPIRED') {
+            Log::info($tpayload->product_id . " GRACE_PERIOD_EXPIRED");
+        }
+
+        // renewal
+        // $signedRenewalInfo = $data->signedRenewalInfo;
+        // list($rheader, $rpayload, $rsignature) = explode('.', $signedRenewalInfo);
+
+        // $rheader = JWT::jsonDecode(JWT::urlsafeB64Decode($rheader));
+        // $rpayload = JWT::jsonDecode(JWT::urlsafeB64Decode($rpayload));;
+
+        // if ($header == $rheader) {
+        //     Log::info("Renewal header is the same");
+        // }
+
+        // Log::info(var_export($rheader, true));
+        // Log::info(var_export($rpayload, true));
+
+        return response()->json([
+            'status' => true,
+            'message' => 'OK'
+        ], 200);
     }
 }
